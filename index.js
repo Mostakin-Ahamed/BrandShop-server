676d0a3c53bdb62156513a8ada6c407db0773ac3
Mostakin-Ahamed <mostakinahamedwork@gmail.com>
2023-10-19 23:59:57 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -11,12 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 
-//automobile-user
-//Um43DXBrdZP7MouL
 
 
 
-const uri = "mongodb+srv://automobile-user:Um43DXBrdZP7MouL@mostakinahamed.fo1obhn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mostakinahamed.fo1obhn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
